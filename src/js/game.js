e5a8b9b14562242fb2b8ade3fe5774e9a9451cb5
robinm50 <robin.moerkens@icloud.com>
2026-06-03 17:51:03 +0200

    remove unused image assets

--- src/js/game.js
+++ src/js/game.js
@@ -21,12 +21,10 @@ export class Game extends Engine {
         fish.pos = new Vector(500, 300)
         fish.vel = new Vector(-10,0)
         fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+        
     }
 
-    fishLeft(e) {
-        e.target.pos = new Vector(1350, 300)
-    }
+    
 }
 
 new Game()
--- src/js/resources.js
+++ src/js/resources.js
@@ -2,8 +2,8 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 
 // voeg hier jouw eigen resources toe
 const Resources = {
-    Fish: new ImageSource('images/fish.png'),
-    Shark: new ImageSource('images/shark.png')
+    // Fish: new ImageSource('images/fish.png'),
+ 
 }
 
 
